a794974efd2615ba47eb5bd5ae5e8a638010b57b
Mage0Foxy <91876215+Mage0Foxy@users.noreply.github.com>
2022-03-30 11:10:27 +0200

    Update Index.cs
    Whooooooooooo!

--- FoxFinder/Index.cs
+++ FoxFinder/Index.cs
@@ -1,7 +1,12 @@
+using MongoDB.Driver;
 namespace FoxFinder
 {
     public partial class Index : Form
     {
+        MongoClient m_Client;
+        IMongoDatabase m_Database;
+        IMongoCollection<Media> m_Collection;
+
         MainPage page;
         Video_s video;
         Books book;
@@ -15,6 +20,10 @@ namespace FoxFinder
             game = new Games();
             favs = new Favs();
             InitializeComponent();
+
+            m_Client = new MongoClient("mongodb+srv://NovaFoxy:Kyuubi99@maincluster.jtr1q.mongodb.net/FoxFinder?retryWrites=true&w=majority");
+            m_Database = m_Client.GetDatabase("FoxFinder");
+            m_Collection = m_Database.GetCollection<Media>("Media");
         }
 
         private void foxfinder_Click(object sender, EventArgs e)
